3bae6982705df034afc9f1e0998226fdaa2f7d1d
sleepyghost-zzz <joshua.azro@gmail.com>
2025-04-21 10:42:52 -0400

    finished front end with updated kinds

--- out/extension.js
+++ out/extension.js
@@ -1,37 +1,67 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-
-export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      'robotvibecoder.sidebar',
-      new SidebarProvider(context),
-      { webviewOptions: { retainContextWhenHidden: true } }
-    )
-  );
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+const vscode = __importStar(require("vscode"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+function activate(context) {
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider('robotvibecoder.sidebar', new SidebarProvider(context), { webviewOptions: { retainContextWhenHidden: true } }));
 }
-
-class SidebarProvider implements vscode.WebviewViewProvider {
-  constructor(private context: vscode.ExtensionContext) {}
-
-  resolveWebviewView(view: vscode.WebviewView) {
-    view.webview.options = { enableScripts: true };
-    view.webview.html = this.getHtml(view.webview);
-
-    view.webview.onDidReceiveMessage(msg => {
-      if (msg.command === 'generate') {
-        const folder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-        if (!folder) return;
-        const filePath = path.join(folder, 'robotvibecoder_config.json');
-        fs.writeFileSync(filePath, JSON.stringify(msg.data, null, 2));
-        vscode.window.showInformationMessage('Config saved!');
-      }
-    });
-  }
-
-  private getHtml(webview: vscode.Webview) {
-	return `
+class SidebarProvider {
+    context;
+    constructor(context) {
+        this.context = context;
+    }
+    resolveWebviewView(view) {
+        view.webview.options = { enableScripts: true };
+        view.webview.html = this.getHtml(view.webview);
+        view.webview.onDidReceiveMessage(msg => {
+            if (msg.command === 'generate') {
+                const folder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+                if (!folder)
+                    return;
+                const filePath = path.join(folder, 'robotvibecoder_config.json');
+                fs.writeFileSync(filePath, JSON.stringify(msg.data, null, 2));
+                vscode.window.showInformationMessage('Config saved!');
+            }
+        });
+    }
+    getHtml(webview) {
+        return `
 	  <!DOCTYPE html>
 	  <html lang="en">
 	  <head>
@@ -201,10 +231,7 @@ class SidebarProvider implements vscode.WebviewViewProvider {
 	  </body>
 	  </html>
 	`;
+    }
 }
-
-
-  
-}
-
-export function deactivate() {}
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
